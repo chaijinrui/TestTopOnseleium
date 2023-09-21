88f390393559052755283e8bdea6ff13aefcb52d
sns-chaijinrui <chai0304@163.com>
2023-09-21 18:44:32 +0800

    111

--- src/main/java/org/example/Test2.java
+++ src/main/java/org/example/Test2.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -24,7 +25,20 @@ public class Test2 {
         chromeDriver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/form/div[1]/div/div/div[1]/input")).sendKeys("xunshitiankong@163.com");
         chromeDriver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/form/div[2]/div/div/div[1]/input")).sendKeys("Bj@116118");
         chromeDriver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/form/div[4]/div")).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+        chromeDriver.findElementByXPath("//*[@id=\"app\"]/div[2]/div[1]/div/div/ul/div[4]/a").click();
+        Thread.sleep(3000);
+        chromeDriver.findElementByXPath("//*[@id=\"app\"]/div[2]/div[2]/section/div/div[1]/div[1]/div/div/div/div[2]/div/div/span/span/div/div/div/div/div/div/span").click();
+        Thread.sleep(3000);
+//        选择应用
+//        chromeDriver.findElementByClassName("el-input__inner").sendKeys("el-input__inner");
+//        搜索
+        chromeDriver.findElementByClassName("label-box").click();
+//        adx开关
+        chromeDriver.findElementByXPath("//*[@id=\"app\"]/div[2]/div[2]/section/div/div[2]/div/div/div[3]/div/div/div/div/div[1]/table/tbody[1]/tr[2]/td[4]/div/div/div/div/span").click();
+        //*[@id="app"]/div[2]/div[2]/section/div/div[2]/div/div/div[3]/div/div/div/div/div[1]/table/tbody[1]/tr[2]/td[4]/div/div/div/div/span
+        Thread.sleep(3000);
+
         chromeDriver.quit();
         long endTime = System.currentTimeMillis();
         System.out.println("执行时长=" + (endTime - startTime) / 1000.00 + "s");
